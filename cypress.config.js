72eac55cab36fddeb5859f2ca5410dc2d03f591a
ElamuruganGR <elamurugan96@gmail.com>
2022-07-12 10:47:16 +0530

    Added plugin to loose import

--- cypress.config.js
+++ cypress.config.js
@@ -9,8 +9,32 @@ module.exports = defineConfig({
 
   component: {
     devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+      framework: 'create-react-app',
+      bundler: 'webpack',
+      webpackConfig: {
+        mode: 'development',
+        devtool: false,
+        module: {
+          rules: [
+            {
+              test: /\.?js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react'],
+                  plugins: [
+                    [
+                      '@babel/plugin-transform-modules-commonjs',
+                      { loose: true },
+                    ],
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      },
     },
-  },
+  }
 });
--- cypress/component/Button.cy.js
+++ cypress/component/Button.cy.js
@@ -1,6 +1,7 @@
 import { Button } from "../../src/Button";
 import { mount } from "@cypress/react";
 import * as UC from "../../src/useChange";
+import React from "react";
 
 describe("Button.jsx", () => {
   // The scenarios to be tested:
@@ -16,12 +17,12 @@ describe("Button.jsx", () => {
   it("stub", () => {
     cy.stub(UC, "useChange").returns([true, cy.stub().as("setter1")]);
     mount(<Button />).then(() => {
-      cy.get("button").should("have.text", "Add me");
+      cy.get("button").should("have.text", "Remove me");
     }).then(() => {
-      // cy.stub(UC, "useChange").returns([false, cy.stub().as("setter2")]);
+      cy.stub(UC, "useChange").returns([false, cy.stub().as("setter2")]);
       cy.get("button").click();
       cy.get("@setter1").should("have.been.calledOnce");
-      cy.get("button").should("have.text", "Remove me");
+      cy.get("button").should("have.text", "Add me");
     });
   });
 });
